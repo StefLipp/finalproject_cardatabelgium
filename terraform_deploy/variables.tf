7d13297fda81aff56ce1a29e1b8c3ae13d92aa11
StefLipp <64265846+StefLipp@users.noreply.github.com>
2024-09-21 21:05:59 +0200

    terraform setup

---
+++ terraform_deploy/variables.tf
@@ -0,0 +1,69 @@
+variable "app_name" {
+  type        = string
+  description = "Application Name"
+  default     = "cardatabelgium"
+}
+
+variable "container_cpu" {
+  description = "Container cpu"
+  default     = "2000m"
+}
+
+variable "container_memory" {
+  description = "Container memory"
+  default     = "2G"
+}
+
+variable "project_id" {
+  type        = string
+  description = "The name of the project"
+  default     = "carprojectbelgium"
+}
+
+variable "region" {
+  type        = string
+  description = "The default compute region"
+  default     = "us-west2"
+}
+
+variable "zone" {
+  type        = string
+  description = "The default compute zone"
+  default     = "us-west2-a"
+}
+
+variable "repository" {
+  type        = string
+  description = "The name of the Artifact Registry repository to be created"
+  default     = "mage-data-prep"
+}
+
+variable "database_user" {
+  type        = string
+  description = "The username of the Postgres database."
+  default     = "mageuser"
+}
+
+variable "database_password" {
+  type        = string
+  description = "The password of the Postgres database."
+  sensitive   = true
+}
+
+variable "docker_image" {
+  type        = string
+  description = "The docker image to deploy to Cloud Run."
+  default     = "us-central1-docker.pkg.dev/carprojectbelgium/my-new-repo/mageai-customsparkfile:tag1"
+}
+
+variable "domain" {
+  description = "Domain name to run the load balancer on. Used if `ssl` is `true`."
+  type        = string
+  default     = ""
+}
+
+variable "ssl" {
+  description = "Run load balancer on HTTPS and provision managed certificate with provided `domain`."
+  type        = bool
+  default     = false
+}
